9f777f27aeaf7c40c70cea7f872c8a101d141f8b
M. A. Muhaimin Sakib <s4k1b13@gmail.com>
2018-06-02 17:41:15 +0600

    browsignPage modified

--- BrowsingPage/BatchSearch/batchSearch.component.php
+++ BrowsingPage/BatchSearch/batchSearch.component.php
@@ -1,3 +1,25 @@
+<?php
+        function showBatches ($showBatchItem, $term, $courseBefore, $level) {
+        $showBatchItem = sprintf("%02d", $showBatchItem);
+        $ans = "";
+        for($iter = 0; $iter < count($courseBefore[$level-1][$term]); $iter++) {
+            $ans.="<li class = 'items'>
+                <a href='../BrowsingContent/browsingContent.component.php?batchSelect={$showBatchItem}'>{$courseBefore[$level-1][$term][$iter]}</a>
+            </li>";
+        }
+
+        $rt="
+        <ul class = 'itemShow'>
+            <div class = 'itemHeading'>
+                <i class='material-icons'>library_books</i>
+                <h3>Batch {$showBatchItem}</h3>
+            </div>".$ans."
+
+        </ul>
+        ";
+        return $rt;
+    }
+?>
 <div class = 'batchSearch'>
     <p style="color: red">Can not find desired batch?</p>
     <p style="color: #00209F">You can select your desired batch from here ..</p>
@@ -28,8 +50,7 @@
             if($chosenBatch == 0){
             }
             else{
-                $showBatchItem = $chosenBatch;
-                include('ShowBatch/showBatch.component.php');
+                echo showBatches($chosenBatch, 1, $courseBefore, 1);
             }
         ?>
     </div>
