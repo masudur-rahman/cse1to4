39a9a88ebfda83a6382b215b11ccfb12fe00fd98
M. A. Muhaimin Sakib <s4k1b13@gmail.com>
2018-06-22 23:08:08 +0600

    project material page

---
+++ ProjectMaterials/TopNav/topNav.component.php
@@ -0,0 +1,28 @@
+<div id = 'topNav'>
+    <div id = 'descriptions'>
+        <div id = 'hudai'>
+            <div id = 'mainHudai'></div>
+            <div id = 'filterTeacher'>
+                <div>T</div>
+                <div>E</div>
+                <div>A</div>
+                <div>C</div>
+                <div>H</div>
+                <div>E</div>
+                <div>R</div>
+                <div>S</div>
+            </div>
+        </div>
+        <div id = 'filterTags'>
+            <div>T</div>
+            <div>O</div>
+            <div>P</div>
+            <div>I</div>
+            <div>C</div>
+            <div>S</div>
+        </div>
+    </div>
+    <div id = 'tags'>
+
+    </div>
+</div>
---
+++ ProjectMaterials/projectMaterials.component.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project Materials</title>
+
+    <!-- INITIAL-->
+    <link rel="stylesheet" type="text/css" href="projectMaterials.component.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Styles/css/cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <script src = "../Styles/js/jquery-3.1.1.js" type="text/javascript"></script>
+    <script src = "../Styles/js/tether.min.js" type="text/javascript"></script>
+    <script src = "../Styles/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!-- NAVBAR CSS -->
+    <link rel="stylesheet" type="text/css" href="../NavigationBar/navBar.component.css">
+
+    <!-- SIDENAV -->
+    <link rel="stylesheet" type="text/css" href="SideNav/sideNav.component.css">
+
+    <!-- TOPNAV -->
+    <link rel="stylesheet" type="text/css" href="TopNav/topNav.component.css">
+
+
+
+</head>
+
+<body>
+    <?php include("../NavigationBar/navBar.component.php"); ?>
+    <?php include("TopNav/topNav.component.php"); //FOR FILTERING BY TAGS ?>
+    <?php include("SideNav/sideNav.component.php"); //FOR SHOWING TECHERS NAME ?>
+</body>
+
+</html>
