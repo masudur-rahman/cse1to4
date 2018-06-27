8307377495dcfc24f6b1e53b97008fc609c8f633
M. A. Muhaimin Sakib <s4k1b13@gmail.com>
2018-06-27 19:59:48 +0600

    new project page

---
+++ ProjectMaterials/ShowProjects/Projects/project.component.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="project.component.css">
+
+<div class = 'project'>
+    <div class = 'projectIcon'>
+        <div class = 'iconP'>
+        </div>
+    </div>
+    <div class = 'projectDescription'>
+        <div class = 'projectTitle' title="CSE Final Year Project 1">
+            CSE Final Year Project 1fasdfskjfhjkashdfklsdhfla
+        </div>
+
+    </div>
+</div>
